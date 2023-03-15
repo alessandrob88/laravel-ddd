fc6d1de60eee833ab9434df682c344b40370eb4d
Alessandro Bocci <a.bocci@doveconviene.com>
2023-03-15 19:01:59 +0100

    feat: register event listeners

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,14 @@
 
 namespace App\Providers;
 
+use App\Events\InvoiceCancel;
+use App\Events\InvoiceCreate;
+use App\Events\InvoiceRowCreate;
+use App\Events\InvoiceRowUpdate;
+use App\Listeners\InvoiceCancelEventListener;
+use App\Listeners\InvoiceCreateEventListener;
+use App\Listeners\InvoiceRowCreateEventListener;
+use App\Listeners\InvoiceRowUpdateEventListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -18,6 +26,18 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        InvoiceCreate::class => [
+            InvoiceCreateEventListener::class,
+        ],
+        InvoiceCancel::class => [
+            InvoiceCancelEventListener::class,
+        ],
+        InvoiceRowCreate::class => [
+            InvoiceRowCreateEventListener::class,
+        ],
+        InvoiceRowUpdate::class => [
+            InvoiceRowUpdateEventListener::class,
+        ]
     ];
 
     /**
