799cb7c562d0fd89aa58845473e52d218cd00a20
Alessandro Bocci <a.bocci@doveconviene.com>
2023-03-19 17:53:45 +0100

    fix: typo

--- app/Listeners/InvoiceRowCreateEventListener.php
+++ app/Listeners/InvoiceRowCreateEventListener.php
@@ -3,6 +3,7 @@ namespace App\Listeners;
 
 use App\Domain\Invoice\ValueObjects\InvoicePayload;
 use App\Events\InvoiceRowCreate;
+use App\Jobs\InvoiceRowCreateJob;
 use Illuminate\Support\Facades\Log;
 
 class InvoiceRowCreateEventListener
@@ -13,7 +14,7 @@ class InvoiceRowCreateEventListener
     public function handle(InvoiceRowCreate $event): void
     {
         Log::info('Invoice row create event received');
-        InvoiceRowCreate::dispatch(
+        InvoiceRowCreateJob::dispatch(
             InvoicePayload::fromArray($event->payload)
         );
     }
